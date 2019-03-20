a5cc642f0a8f50d0936b05ea5c6945e633fa9eef
iscoct <frandkv27@gmail.com>
2019-03-20 19:30:21 +0100

    Añadido los ficheros de las partes separadas

---
+++ barraLateral.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<aside class="borde" id="navegacionLateral">
+    <ul id="listaLateral">
+        <li class="elementoLateral">Check-in</li>
+        <li class="elementoLateral">Tus reservas</li>
+        <li class="elementoLateral">Estados vuelos</li>
+        <li class="elementoLateral"><a href="evento.html">Evento</a></li>
+    </ul>
+</aside>";
+?>
---
+++ cabecera.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<header class="borde" id="cabecera">
+            <a href="principal.html"><img id="imagenAvion" src="img/avion.jpg" alt="Logotipo de la agencia de viajes"/></a>
+            <h1 id="cabeceraPrincipal">Viajes</h1>
+            <nav>
+                <ul id="menuCabecera">
+                    <li class="elementoMenu">Viaja</li>
+                    <li class="elementoMenu">Tus vuelos</li>
+                    <li class="elementoMenu">Antes de viajar</li>
+                    <li class="elementoMenu"><a href="principal.html">Principal</a></li>
+                </ul>
+            </nav>
+        </header>";
+?>
