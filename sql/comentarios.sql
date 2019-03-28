5c5ecefb095cac792271c671aa17e52025cd0c84
Francisco José Cotán López <iscoct@correo.ugr.es>
2019-03-28 16:50:35 +0100

    Añadido campos en la base de datos y formato de las imagenes en principal.css

--- sql/comentarios.sql
+++ sql/comentarios.sql
@@ -1,28 +1,31 @@
 create table if not exists Comentarios (
 	idViaje varchar(64),
+	ipUtilizada varchar(64),
 	nombreAutor varchar(64),
 	fecha date,
 	hora time,
 	email varchar(128),
 	texto varchar(256),
 		PRIMARY KEY(nombreAutor, fecha, hora),
-		FOREIGN KEY(idViaje) references Viajes(id)
+		FOREIGN KEY(idViaje) references Viajes(id) on delete cascade
 );
 
 insert into Comentarios values (
 	"0",
+	"123.123.123.123",
 	"Francisco José Cotán López",
 	date("1996-07-16"),
 	"12:30:00",
-	NULL,
+	"frandkv27@gmail.com",
 	"Hola mi nombre es Fran"
 );
 
 insert into Comentarios values (
 	"0",
+	"123.123.123.123",
 	"Pedro Luis Fuertes",
 	date("2019-3-22"),
 	"16:00:00",
-	NULL,
+	"frandkv27@gmail.com",
 	"Hola mi nombre es Pedro"
 );
